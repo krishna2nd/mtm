1feaabf1a09cd102a7502ab14f791fc93d5ebc2d
Anand Didwania <andidwan@microsoft.com>
2019-11-20 10:39:41 +0530

    triggers db

--- src/middlewares/triggers/index.ts
+++ src/middlewares/triggers/index.ts
@@ -1,27 +1,27 @@
 const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('./tags.db', sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database('./triggers.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
       return console.error(err.message);
     }
     console.log('Connected to the in-memory SQlite database.');
   });
 
-const URL_BASE: string = "/tags";
+const URL_BASE: string = "/triggers";
 
 export default (app) => {
     app.get(URL_BASE, (req, res) => {
-        res.send("Tags GET")
+        res.send("Triggers GET")
     });
     app.get(`${URL_BASE}/:id`, (req, res) => {
-        res.send("Tags Some  GET")
+        res.send("Trigger Some  GET")
     });
     app.put(URL_BASE, (req, res) => {
         console.log(req.body);
         res.send(req.body);
     });
     app.post(`${URL_BASE}/:id`, (req, res) => {
-        res.send("Update new tag")
+        res.send("Update new trigger")
     });
 };
 
--- src/models/MTMTrigger.ts
+++ src/models/MTMTrigger.ts
@@ -1,3 +1,7 @@
+
+import MTMDataStore from "./db/MTMDataStore";
+import ModelQueryStatement, { IQueryStatement } from "./db/ModelQueryStatement";
+
 export interface ITriggerItem
  {
     name: string;
@@ -5,12 +9,14 @@ export interface ITriggerItem
     selector?: string;
     body?: string;
     lastEdited: Date;
-
-    get(id:string): ITriggerItem;
-    create(tag: ITriggerItem): ITriggerItem;
-    delete(tag: ITriggerItem): boolean;
-    update(tag: ITriggerItem): ITriggerItem;
 }
+
+export interface IMTMTriggerItem extends ITriggerItem {
+    get(id: string): Promise<ITriggerItem>;
+    create(tag: ITriggerItem): Promise<ITriggerItem>;
+    delete(tag: ITriggerItem): Promise<boolean>;
+    update(tag: ITriggerItem): Promise<ITriggerItem>;
+};
 
 export enum TriggerType {
     CLICK ='click',
@@ -18,17 +24,39 @@ export enum TriggerType {
     PAGE_LOAD = 'page_load'
 }
 
-export class MTMTriggerItem implements ITriggerItem {
+export class MTMTriggerItem implements IMTMTriggerItem {
     public name: string;
     public type: TriggerType;
     public selector?: string;
     public body?: string;
     public lastEdited: Date;
-    public get(id:string) { return {} as ITriggerItem; }
-    public create(tag: ITriggerItem) { return tag; }
-    public delete(tag: ITriggerItem) { return true; }
-    public update(tag: ITriggerItem) { return tag; }
-    static getAll() { return Array<ITriggerItem>();}
+    static query: ModelQueryStatement;
+    static store: MTMDataStore;
+    public async get(id:string): Promise<ITriggerItem> { 
+        const value: any = await MTMTriggerItem.store.Select(MTMTriggerItem.query.SELECT, id)
+        return JSON.parse(value.data) as ITriggerItem; 
+    }
+    public async create(trigger: ITriggerItem) { 
+        const value: any = MTMTriggerItem.store.Insert(MTMTriggerItem.query.INSERT, JSON.stringify(trigger));
+        return await value;
+     }
+    public async delete(tag: ITriggerItem) { return true; }
+    public async update(tag: ITriggerItem) { return tag; }
+    static async getAll(): Promise<Array<ITriggerItem>> {
+        const value: any = await MTMTriggerItem.store.SelectAll(this.query.SELECT_ALL)
+        return JSON.parse(value.data) as Array<ITriggerItem>
+    }
+
+    constructor() {
+        MTMTriggerItem.query = new ModelQueryStatement({
+            SELECT: "SELECT * FROM TRIGGERS WHERE ID=?",
+            SELECT_ALL: "SELECT * FROM TRIGGERS",
+            INSERT: `INSERT INTO TRIGGERS ("DATA") VALUES (?)`,
+            DELETE: `DELETE FROM TRIGGERS WHERE ID=?`,
+            UPDATE: `UPDATE "DATA` 
+        } as IQueryStatement);
+        MTMTriggerItem.store = MTMDataStore.getInstance();
+    }
 }
 
 export default MTMTriggerItem;
