7c1a7ff117c0d572b2a22d769f9e511a8ebd5e1b
Krishna Kumar <krk@microsoft.com>
2019-11-20 19:04:05 +0530

    Updated config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,12 @@ module.exports = {
   apps: [{
     name: "MTM Server",
     script: "dist/index.js",
-    watch: true,
-    ignore_watch: ["node_modules"],
-    instances: 1,
+    instances: "max",
     autorestart: true,
+    exec_mode : "cluster",
     max_memory_restart: '1G',
     env: {
-      NODE_ENV: 'development'
+      NODE_ENV: 'production'
     },
     env_production: {
       NODE_ENV: 'production'
@@ -21,8 +20,8 @@ module.exports = {
       host: '0.0.0.0',
       ref: 'origin/master',
       repo: 'git@github.com:repo.git',
-      path: '/var/www/production',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+      path: '/home/site',
+      'post-deploy': 'pm2 reload ecosystem.config.js --env production'
     }
   }
 };
